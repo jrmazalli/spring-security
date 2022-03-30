2f8b7128ec45370bed49c0d1f58c40a69ae4b0fd
Junior Mazalli <junior.mazalli@afgbrasil.com.br>
2022-03-30 13:40:56 -0300

    configure adapter

--- src/main/java/dio/spring/security/WelcomeController.java
+++ src/main/java/dio/spring/security/WelcomeController.java
@@ -1,6 +1,5 @@
 package dio.spring.security;
 
-import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -11,12 +10,12 @@ public class WelcomeController {
         return "Welcome to My Spring Boot Web API";
     }
     @GetMapping("/users")
-    @PreAuthorize("hasAnyRole('MANAGERS','USERS')")
+    //@PreAuthorize("hasAnyRole('MANAGERS','USERS')")
     public String users() {
         return "Authorized user";
     }
     @GetMapping("/managers")
-    @PreAuthorize("hasRole('MANAGERS')")
+   // @PreAuthorize("hasRole('MANAGERS')")
     public String managers() {
         return "Authorized manager";
     }
